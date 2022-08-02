37cf20eb77d264cf076309de11651a109a19f758
DESKTOP-BH99S7S\PC - Themefic <hemelsrgmts@gmail.com>
2022-08-02 17:20:33 +0600

    Info Details Add those pages-about,hero

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['JavaScript (ES6+)', 'React', 'Node.js', 'BlockChain', 'WordPress', 'GSAP'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,21 +135,15 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+            Hello! My name is M Hemel Hasan and I enjoy creating things on the internet. My interest in web development started back in 2010 when I decided to try convert PSD to HTML themes — and making free themes for blogger, That taught me a lot about HTML &amp; CSS at the same time!
             </p>
 
             <p>
               Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              computer software solution,{' '}
+              Desh Team,{' '}
+              SoftTech, and{' '}
+              And UpWork. My main focus these days is to create affordable, international quality products and digital experiences for various clients.
             </p>
 
             <p>
--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,15 +60,13 @@ const Hero = () => {
 
   const one = <h1>Hi, my name is</h1>;
   const two = <h2 className="big-heading">M Hemel Hasan.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const three = <h3 className="big-heading">I make things for the web.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+          I'm a software engineer specializing in creating exceptional digital experiences (WordPress, React, Blockchain). I am currently working on Blockchain and WordPress. My front-end specialty is GSAP {' '}
+        <a href="https://mhemelhasan.com/" target="_blank" rel="noreferrer">
+          Check My Work
         </a>
         .
       </p>
